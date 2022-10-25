8fb6d2c15fd09546c8bdc693ad1d79d6ca5ddfcc
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-25 12:34:36 +0300

    Implement factorial of a number solution

--- factorial.js
+++ factorial.js
@@ -10,6 +10,16 @@
     factorial(5) = 5 * 4 * 3 * 2 * 1 = 120 
 */
 
-function factorial() {
+function factorial(n) {
+    let result = 1;
 
+    for (let i = 2; i <= n; i++) { // O(n)
+        result *= i;
+    }
+
+    return result;
 }
+
+console.log(factorial(0)); // 1
+console.log(factorial(1)); // 1
+console.log(factorial(5)); // 120
