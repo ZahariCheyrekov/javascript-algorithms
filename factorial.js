bd6b066b551f62a41aa976e59e4c9d2681c53fed
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-10-25 12:29:55 +0300

    Add factorial problem description

---
+++ factorial.js
@@ -0,0 +1,15 @@
+/* 
+    Problem - Give an integer 'n', find the factorial of that integer.
+    
+    In mathematics, the factorial of a non-integer 'n',
+    denoted n!, is the product of all positive integers less tha or equal to 'n'.
+
+    Factorial of zero is 1.
+
+    factorial(4) = 4 * 3 * 2 * 1 = 24 
+    factorial(5) = 5 * 4 * 3 * 2 * 1 = 120 
+*/
+
+function factorial() {
+
+}
